8e0e79c2b823667f714a4083ffddcfa75a495805
muneeb <muneeburrehman501@gmail.com>
2024-12-11 10:40:06 -0800

    some chnages made to app.js needed for testing

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
+const mongoose = require('mongoose');
 const app = require('./app'); 
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const db = require('./config/db');
+mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('MongoDB connected');
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error('Failed to connect to MongoDB:', err);
+    process.exit(1); 
+  });
